ba132e3b06ea4265fad9056b7fcaaac60f458afd
RZXBxie <2545147971@qq.com>
2024-10-23 17:25:12 +0800

    feat: day03--method

--- day03/src/Main.java
+++ day03/src/Main.java
@@ -4,6 +4,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("数组练习");
         arrayTest();
+        System.out.println(getSum(1, 2));
+        System.out.println(getSum(1, 3, 4));
+
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6, 7};
+        int[] ans = copyOfRange(nums, 0, 3);
+        for (int num : ans) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static int[] copyOfRange(int[] nums, int start, int end) {
+        int[] ans = new int[end - start];
+        for (int i = start; i < end; i++) {
+            ans[i] = nums[i];
+        }
+        return ans;
+    }
+
+    public static int getSum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int getSum(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
     }
 
     public static void arrayTest() {
