1475f64d4d6b92b9ad1d9e739e058bf4f7807345
RZXBXie <90632247+RZXBxie@users.noreply.github.com>
2024-11-12 15:36:00 +0800

    fix: bug

--- Test.java
+++ Test.java
@@ -1,17 +1,8 @@
-import java.util.Scanner;
-
 public class Test {
-	static int a = 10;
-
-	public Test() {
-
-	}
-
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		sc.nextLine();
-
-
+		String s = "hello, world";
+		int a = 10;
+		System.out.println(s == "hello, world");
+		System.out.println(a == 10);
 	}
 }
